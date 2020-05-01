42faed272fa55f60f3fb239bbfb66419a98edb78
LAMHOYIN-2020 <61041997+LAMHOYIN-2020@users.noreply.github.com>
2020-05-01 18:15:20 +0800

    Update form.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,9 +8,9 @@ from app.models import User
 
 class EditProfileForm(FlaskForm):
     username = StringField(_l('Username'), validators=[DataRequired()])
-    about_me = TextAreaField(_l('About me'),
-                             validators=[Length(min=0, max=140)])
-    submit = SubmitField(_l('Submit'))
+    about_me = TextAreaField(_l('About me-upload area'),
+                             validators=[Length(min=10, max=1000)])
+    submit = SubmitField(_l('Upload'))
 
     def __init__(self, original_username, *args, **kwargs):
         super(EditProfileForm, self).__init__(*args, **kwargs)
@@ -24,5 +24,21 @@ class EditProfileForm(FlaskForm):
 
 
 class PostForm(FlaskForm):
-    post = TextAreaField(_l('Say something'), validators=[DataRequired()])
-    submit = SubmitField(_l('Submit'))
+    post = TextAreaField(_l('Upload article'), validators=[DataRequired()])
+    submit = SubmitField(_l('Upload'))
+
+
+class RootForm(FlaskForm):
+    customers = StringField(_l('customers'), validators=[DataRequired()])
+    business = StringField(_l('business'), validators=[DataRequired()])
+    other = StringField(_l('other'), validators=[DataRequired()])
+    submit = SubmitField(_l('Update'))
+
+
+class CareersForm(FlaskForm):
+    WorkLocationinclude = StringField(_l('Work Location Include'), validators=[DataRequired()])
+    CareerCategory = StringField(_l('Career Category'), validators=[DataRequired()])
+    JobTitle = StringField(_l(' Job Title'), validators=[DataRequired()])
+    Category = StringField(_l('Category'), validators=[DataRequired()])
+    Location = StringField(_l('Location'), validators=[DataRequired()])
+    submit = SubmitField(_l('Update Careers'))
